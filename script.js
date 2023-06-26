75d71bbe61a124b75124faa9cf4a424c148fae4c
Halim258 <mohamed.halme204204@gmail.com>
2023-06-26 16:02:07 +0200

    fixx burger icon

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const initApp = () => {
   const hamburger = document.querySelector('.hamburger');
-  const topLinks = document.querySelector('.Higher-links');
+  const topLinks = document.querySelector('.higher-links');
 
   hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     topLinks.classList.toggle('active');
   });
 
-  document.querySelectorAll('.Higher-link').forEach((n) => n.addEventListener('click', () => {
+  document.querySelectorAll('.higher-link').forEach((n) => n.addEventListener('click', () => {
     hamburger.classList.remove('active');
     topLinks.classList.remove('active');
   }));
